4d69baf3e914877c4c4bc826dd30dbd2f6018e7f
renata.2805 <renata.soria.arg@gmail.com>
2022-06-24 21:20:45 -0300

    Agregado consigna 3

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ("express");
+const app = express();
+app.use (express.static("public"));
+app.listen(3000, ()=> {
+    console.log("Servidor Funcionando")
+});
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+});
+
