3104de9952bdd0ed1acaba24f9f0a4052ec79d16
xGorgory <santiagores130@gmail.com>
2022-06-30 22:41:21 -0500

    modificacion App.js

--- app.js
+++ app.js
@@ -14,4 +14,25 @@ app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname + "/views/index.html"));
 });
 
+app.get("/login.html",(req,res)=>{
+    res.sendFile(path.resolve(__dirname + "/views/login.html"));
+});
+
+app.get("/register.html",(req,res)=>{
+    res.sendFile(path.resolve(__dirname + "/views/register.html"));
+});
+
+app.get("/productCard.html",(req,res)=>{
+    res.sendFile(path.resolve(__dirname + "/views/productCard.html"));
+});
+
+
+app.get("/productDetail.html",(req,res)=>{
+    res.sendFile(path.resolve(__dirname + "/views/productDetail.html"));
+});
+
+app.post("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname + "/views/index.html"));
+});
+
 
