257b28466e3f64558b5426ff1930267b53d7c00d
sebadg9 <diaz.sebastian1988@gmail.com>
2022-07-30 19:08:49 -0300

    agregando carpetas routes y controllers y construyendo template engines

---
+++ controllers/mainController.js
@@ -0,0 +1,31 @@
+// const controller = {
+    
+//     index: (req, res) => {
+//         return res.render ('index'); // como parametros va el nombre del archivo dentro views 
+//    },
+
+//    register: (req, res) => {
+//        return res.render ('register'); //  como parametros va el nombre del archivo dentro views
+//       },
+
+//    login: (req, res) => {
+//        return res.render ('Login'); // como parametros va el nombre del archivo dentro views
+//       },
+  
+//       productCart: (req, res) => {
+//         return res.render ('productCart'); // como parametros va el nombre del archivo dentro views
+//        },
+
+//        productDetail: (req, res) => {
+//         return res.render ('productDetail'); // como parametros va el nombre del archivo dentro views
+//        },
+
+
+
+   
+      
+  
+  
+//    }
+
+//    module.exports= controller;
