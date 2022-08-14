67f1c7303a35344adb439175c9a101eaa9015a7f
renata.2805 <renata.soria.arg@gmail.com>
2022-08-14 12:22:25 -0300

    Form de edición

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const usersController= require('../controllers/usersController');
 router.get ('/register', usersController.register);
 router.get('/login', usersController.login);
 router.get("/users/edit/:idUser", usersController.edit);
-router.put("/edit", function (req,res) {
+router.put("/users/edit", function (req,res) {
     res.send("Modificación exitosa!")
 })
 
