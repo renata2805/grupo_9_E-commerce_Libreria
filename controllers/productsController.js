30d51b196583e276d805c438b36f3640f43622ea
renata.2805 <renata.soria.arg@gmail.com>
2022-08-14 10:19:25 -0300

    Form de edición de usuarios

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+var users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"))
+
 const usersController = {
     register: (req, res) => {
         res.render ('register'); //  como parametros va el nombre del archivo dentro views
@@ -8,6 +14,13 @@ const usersController = {
     createProduct: (req, res) => {
         res.render ('indexAdmin'); // como parametros va el nombre del archivo dentro views
        },
+    edit: function(req,res) {
+      let idUser = req.params.idUser;
+
+      let userToEdit = users[idUser] 
+
+      res.render("userEdit", {userToEdit: userToEdit});
+    }   
 
 }
 
