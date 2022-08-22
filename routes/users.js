78909db19cd86f4e9e1a26f6fa7e6bf4e703a234
renata.2805 <renata.soria.arg@gmail.com>
2022-08-22 20:53:39 -0300

    Form de eliminación de usuarios

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,11 @@ router.get('/login', usersController.login);
 router.get("/users/edit/:idUser", usersController.edit);
 
 router.put("/users/edit", function (req,res) {
-    res.send("Modificación exitosa!")
+    res.send("¡Modificación exitosa!")
 });
+
+router.delete("/users/delete/:idUser", function (req,res){
+    res.send("¡Eliminación exitosa!")
+})
 
  module.exports= router;
