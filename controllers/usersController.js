3eb442b5b4e8f79fe911846bf1ff1d8e645b9737
MaruSiches <marinaisiches@gmail.com>
2022-08-27 02:17:59 -0300

    Implementando funcionamiento del registro nuevo Usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
 var users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
+
 const usersController = {
     register: (req, res) => {
         res.render ('register'); //  como parametros va el nombre del archivo dentro views
@@ -11,15 +12,28 @@ const usersController = {
     login: (req, res) => {
         res.render ('Login'); // como parametros va el nombre del archivo dentro views
       },
-    createProduct: (req, res) => {
-        res.render ('indexAdmin'); // como parametros va el nombre del archivo dentro views
-       },
     edit: function(req,res) {
       let idUser = req.params.idUser;
 
       let userToEdit = users[idUser] 
 
       res.render("userEdit", {userToEdit: userToEdit});
+    },
+    update:  (req, res) => {
+      let imagen
+      if(req.files[0] != undefined){
+        imagen = req.files[0].filename
+      } else {
+        imagen = 'default-image.jpg'
+      }
+      let newUser = {
+        id: users[users.length - 1].id + 1,
+        ...req.body,
+        imagen: imagen
+      };
+      users.push(newUser)
+      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+      res.redirect('index');
     },
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 const express= require('express');
 const router= express.Router();
+const path = require("path");
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null, './public/images/users')
+    },
+    filename: function(req,file,cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+var upload = multer({storage: storage})
 
 const usersController= require('../controllers/usersController'); 
 
 router.get ('/register', usersController.register);
+router.post('/', upload.any(), usersController.update);
 router.get('/login', usersController.login);
 router.get("/users/edit/:idUser", usersController.edit);
 
