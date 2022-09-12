f5667b5af83e1f9396d140be707e95e317d5f1be
renata.2805 <renata.soria.arg@gmail.com>
2022-09-12 01:21:32 -0300

    Register

--- models/User.js
+++ models/User.js
@@ -7,14 +7,14 @@ const User = {
 		return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
 	},
 
-	generateId: function () {
-		let allUsers = this.findAll();
-		let lastUser = allUsers.pop();
-		if (lastUser) {
-			return lastUser.id + 1;
-		}
-		return 1;
-	},
+	// generateId: function () {
+	// 	let allUsers = this.findAll();
+	// 	let lastUser = allUsers.pop();
+	// 	if (lastUser) {
+	// 		return lastUser.id + 1;
+	// 	}
+	// 	return 1;
+	// },
 
 	findAll: function () {
 		return this.getData();
