1dbd7a6873803e3e5ace04aa40fd80298ca7a651
MaruSiches <marinaisiches@gmail.com>
2022-10-31 22:18:23 -0300

    validacion front end

--- public/js/validacion-front.js
+++ public/js/validacion-front.js
@@ -1,17 +1,23 @@
-window.addEventListener('load', function(){
-    let formulario = document.querySelector('form.reservation');
+window.onload = function(){
+    
+    
+    let formulario = document.querySelector('#formulario');
+    let form = document.querySelector('.form')
+
+    form.addEventListener('submit', (e) => {
 
         let errores = [];
-        let campoNombre = document.querySelector("title")
+        let campoNombre = document.querySelector("#title")
+        let descripcion = document.querySelector("#description")
 
-        if(campoNombre.value == ""){
+        console.log(campoNombre)
+        if(formulario.campoNombre.value == ""){
             errores.push("el campo del titulo del libro debe estar completo")
-        } else if(campoNombre.value.length < 5){
+        } else if(formulario.campoNombre.value.length < 5){
             errores.push("el titulo del libro debe tener al menos 5 caracteres")
         }
-        let descripcion = document.querySelector("input.description")
 
-        if(descripcion.value == ""){
+        if(formulario.descripcion.value == ""){
             errores.push("Debes colocar una descripción del libro")
         } else if(descripcion.value.length < 20){
             errores.push("La descripcion del libro debe tener al menos 20 caracteres")
@@ -19,15 +25,17 @@ window.addEventListener('load', function(){
 
         let imagen = document.querySelector("input.image")
 
-        if(descripcion.value == ""){
+        if(formulario.descripcion.value == ""){
             errores.push("Debes colocar una descripción del libro")
-        } else if(descripcion.value.length < 20){
+        } else if(formulario.descripcion.value.length < 20){
             errores.push("La descripcion del libro debe tener al menos 20 caracteres")
         }
-    })
+    
 
     formulario.addEventListener(submit, function(e){
         if(errores.length > 0 ){e.preventDefault();
         let ulErrores = document.querySelector(".errores ul");
         errores.forEach(error =>{ulErrores.innerHTML += `<li>${error}</li>`})}
-})
+}
+
+)})};
