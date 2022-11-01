b765c914656959efb718e6c703574bc06d168906
Sebastian Diaz <diaz.sebastian1988@gmail.com>
2022-10-31 23:23:01 -0300

    validaciones front end

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    let formulario= document.querySelector("#formulario")
+    let form= document.querySelector(".form")
+
+    form.addEventListener('submit', (e) => {
+
+        let ema
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+  })
+   }
--- public/js/validacion-front.js
+++ public/js/validacion-front.js
@@ -9,33 +9,48 @@ window.onload = function(){
         let errores = [];
         let campoNombre = document.querySelector("#title")
         let descripcion = document.querySelector("#description")
+        let imagen= document.querySelector("#image")
 
         console.log(campoNombre)
-        if(formulario.campoNombre.value == ""){
+        if(campoNombre.value == ""){
             errores.push("el campo del titulo del libro debe estar completo")
-        } else if(formulario.campoNombre.value.length < 5){
+        } else if(campoNombre.value.length < 5){
             errores.push("el titulo del libro debe tener al menos 5 caracteres")
         }
 
-        if(formulario.descripcion.value == ""){
+        if(descripcion.value == ""){
             errores.push("Debes colocar una descripción del libro")
         } else if(descripcion.value.length < 20){
             errores.push("La descripcion del libro debe tener al menos 20 caracteres")
         }
 
-        let imagen = document.querySelector("input.image")
+        // let imagen = document.querySelector("input.image")
 
-        if(formulario.descripcion.value == ""){
-            errores.push("Debes colocar una descripción del libro")
-        } else if(formulario.descripcion.value.length < 20){
-            errores.push("La descripcion del libro debe tener al menos 20 caracteres")
-        }
+        // if(descripcion.value == ""){
+        //     errores.push("Debes colocar una descripción del libro")
+        // } else if(formulario.descripcion.value.length < 20){
+        //     errores.push("La descripcion del libro debe tener al menos 20 caracteres")
+        // }
     
 
-    formulario.addEventListener(submit, function(e){
-        if(errores.length > 0 ){e.preventDefault();
-        let ulErrores = document.querySelector(".errores ul");
-        errores.forEach(error =>{ulErrores.innerHTML += `<li>${error}</li>`})}
+//     formulario.addEventListener(submit, function(e){
+//         if(errores.length > 0 ){e.preventDefault();
+//         let ulErrores = document.querySelector(".errores ul");
+//         errores.forEach(error =>{ulErrores.innerHTML += `<li>${error}</li>`})}
+// }
+if (errores.length > 0) {
+    e.preventDefault();
+    let ulErrors = document.querySelector('.errores');
+    ulErrors.classList.add('alert-warning');
+    ulErrors.innerHTML = '';
+    for (let i = 0; i < errores.length; i++) {
+        ulErrors.innerHTML += `<li >  ${errores[i]} </li>`;
+    };
+}else{
+    alert('La validación fué exitosa')
+    form.submit();
 }
 
-)})};
+});
+
+};
