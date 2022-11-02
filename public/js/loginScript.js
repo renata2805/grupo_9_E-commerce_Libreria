b8b8ccc75ca000d021f662c2721ddb5a3282b33c
MaruSiches <marinaisiches@gmail.com>
2022-11-02 09:46:42 -0300

    agregando validacion front-end  al formulario de login

---
+++ public/js/loginScript.js
@@ -0,0 +1,41 @@
+window.onload = function(){
+    let formulario = document.querySelector("#formulario")
+    let form = document.querySelector(".form")
+
+    form.addEventListener('submit', (e) => {
+
+        form.email.focus();
+
+        let errores = [];
+        let email = document.querySelector("#email")
+        let password = documenty.querySelector("#password")
+       
+        let regEmail = /\S+@\S+\.S+/;
+        if (regEmail.test(email.value)) {
+            errores.push("Debes ingresar un email válido");
+        }
+        else if (email.value == '') {
+            errores.push("Debes completar tu email")
+        }
+        else {
+            form.password.focus();
+        }
+
+        if(password.value == ""){
+            errores.push('Debes ingresar tu contraseña')
+        }
+
+        if (errores.length > 0) {
+            e.preventDefault();
+            let ulErrors = document.querySelector('.errores');
+            ulErrors.classList.add('alert-warning');
+            ulErrors.innerHTML = '';
+            for (let i = 0; i < errores.length; i++) {
+                ulErrors.innerHTML += `<li>  ${errores[i]} </li>`;
+            };
+        }else{
+            alert('La validación fué exitosa')
+            form.submit();
+        }
+  })
+   }
--- public/js/productCreateScript.js
+++ public/js/productCreateScript.js
@@ -1,6 +1,4 @@
 window.onload = function(){
-    
-    
     let formulario = document.querySelector('#formulario');
     let form = document.querySelector('.form')
 
@@ -11,7 +9,7 @@ window.onload = function(){
         let descripcion = document.querySelector("#description")
         let imagen= document.querySelector("#image")
 
-        console.log(campoNombre)
+    
         if(campoNombre.value == ""){
             errores.push("el campo del titulo del libro debe estar completo")
         } else if(campoNombre.value.length < 5){
@@ -24,20 +22,7 @@ window.onload = function(){
             errores.push("La descripcion del libro debe tener al menos 20 caracteres")
         }
 
-        // let imagen = document.querySelector("input.image")
 
-        // if(descripcion.value == ""){
-        //     errores.push("Debes colocar una descripción del libro")
-        // } else if(formulario.descripcion.value.length < 20){
-        //     errores.push("La descripcion del libro debe tener al menos 20 caracteres")
-        // }
-    
-
-//     formulario.addEventListener(submit, function(e){
-//         if(errores.length > 0 ){e.preventDefault();
-//         let ulErrores = document.querySelector(".errores ul");
-//         errores.forEach(error =>{ulErrores.innerHTML += `<li>${error}</li>`})}
-// }
 if (errores.length > 0) {
     e.preventDefault();
     let ulErrors = document.querySelector('.errores');
